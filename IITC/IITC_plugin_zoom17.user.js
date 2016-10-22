b47dba0b9d726df1fcbf29a8a8ec9a20e4e27c77
megazet <ilol@ya.ru>
2016-10-22 23:48:34 +0500

    Update IITC_plugin_zoom17.user.js

--- IITC/IITC_plugin_zoom17.user.js
+++ IITC/IITC_plugin_zoom17.user.js
@@ -4,8 +4,8 @@
 // @category       Tweaks
 // @version        0.1.0.20161022.0
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      
-// @downloadURL    
+// @updateURL      https://raw.githubusercontent.com/megazet/megazet.github.io/master/IITC/IITC_plugin_zoom17.meta.js
+// @downloadURL    https://raw.githubusercontent.com/megazet/megazet.github.io/master/IITC/IITC_plugin_zoom17.user.js
 // @description    Adds a button to activate the zoom17 mode at any map zoom
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
